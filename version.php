ccc637d059c0ea1158a85e579de455b29d80d794
Chad Mazilly <chadmazilly@gmail.com>
2015-06-24 20:28:02 +0000

    just testing

--- version.php
+++ version.php
@@ -9,4 +9,5 @@ $plugin->version = 2015122912;
 //>>>>>>> 3b143fc7360cf1d74ce5085b737f231a72536558
 $plugin->requires = 2013051400;
 $plugin->release = "v1.5.0";
-$plugin->maturity = MATURITY_STABLE; 
+$plugin->maturity = MATURITY_STABLE;
+//
