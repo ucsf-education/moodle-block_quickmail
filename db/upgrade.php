91c86cc73ded2d0690aba529b54dd0d4f46c3e30
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2024-05-28 11:07:29 -0700

    Corrects PHP linter warnings in /db files.

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,52 +1,88 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-function xmldb_block_quickmail_upgrade($oldversion) {
+/**
+ * Quickmail upgrade script.
+ *
+ * @package    block_quickmail
+ * @copyright  2008-2017 Louisiana State University
+ * @copyright  2008-2017 Adam Zapletal, Chad Mazilly, Philip Cali, Robert Russo
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Run all Quickmail upgrade steps between the current DB version and the current version on disk.
+ * @param int $oldversion The old version of quickmail in the DB.
+ * @return bool
+ * @throws coding_exception
+ * @throws ddl_exception
+ * @throws ddl_field_missing_exception
+ * @throws ddl_table_missing_exception
+ * @throws dml_exception
+ * @throws downgrade_exception
+ * @throws moodle_exception
+ * @throws upgrade_exception
+ */
+function xmldb_block_quickmail_upgrade($oldversion): bool {
     global $DB;
 
     $result = true;
 
     $dbman = $DB->get_manager();
 
-    // 1.9 to 2.0 upgrade
+    // 1.9 to 2.0 upgrade.
     if ($oldversion < 2011021812) {
-        // Changing type of field attachment on table block_quickmail_log to text
+        // Changing type of field attachment on table block_quickmail_log to text.
         $table = new xmldb_table('block_quickmail_log');
         $field = new xmldb_field('attachment', XMLDB_TYPE_TEXT, 'small', null, XMLDB_NOTNULL, null, null, 'message');
 
-        // Launch change of type for field attachment
+        // Launch change of type for field attachment.
         $dbman->change_field_type($table, $field);
 
-        // Rename field timesent on table block_quickmail_log to time
+        // Rename field timesent on table block_quickmail_log to time.
         $table = new xmldb_table('block_quickmail_log');
         $field = new xmldb_field('timesent', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0', 'format');
 
-        // Conditionally launch rename field timesent
+        // Conditionally launch rename field timesent.
         if ($dbman->field_exists($table, $field)) {
             $dbman->rename_field($table, $field, 'time');
         }
 
-        // Define table block_quickmail_signatures to be created
+        // Define table block_quickmail_signatures to be created.
         $table = new xmldb_table('block_quickmail_signatures');
 
-        // Adding fields to table block_quickmail_signatures
+        // Adding fields to table block_quickmail_signatures.
         $table->add_field('id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
         $table->add_field('userid', XMLDB_TYPE_INTEGER, '11', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0');
         $table->add_field('title', XMLDB_TYPE_CHAR, '125', null, null, null, null);
         $table->add_field('signature', XMLDB_TYPE_TEXT, 'medium', null, null, null, null);
         $table->add_field('default_flag', XMLDB_TYPE_INTEGER, '1', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '1');
 
-        // Adding keys to table block_quickmail_signatures
+        // Adding keys to table block_quickmail_signatures.
         $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
 
-        // Conditionally launch create table for block_quickmail_signatures
+        // Conditionally launch create table for block_quickmail_signatures.
         if (!$dbman->table_exists($table)) {
             $dbman->create_table($table);
         }
 
-        // Define table block_quickmail_drafts to be created
+        // Define table block_quickmail_drafts to be created.
         $table = new xmldb_table('block_quickmail_drafts');
 
-        // Adding fields to table block_quickmail_drafts
+        // Adding fields to table block_quickmail_drafts.
         $table->add_field('id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
         $table->add_field('courseid', XMLDB_TYPE_INTEGER, '11', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0');
         $table->add_field('userid', XMLDB_TYPE_INTEGER, '11', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0');
@@ -57,32 +93,32 @@ function xmldb_block_quickmail_upgrade($oldversion) {
         $table->add_field('format', XMLDB_TYPE_INTEGER, '3', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '1');
         $table->add_field('time', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null, null, null);
 
-        // Adding keys to table block_quickmail_drafts
+        // Adding keys to table block_quickmail_drafts.
         $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
 
-        // Conditionally launch create table for block_quickmail_drafts
+        // Conditionally launch create table for block_quickmail_drafts.
         if (!$dbman->table_exists($table)) {
             $dbman->create_table($table);
         }
 
-        // Define table block_quickmail_config to be created
+        // Define table block_quickmail_config to be created.
         $table = new xmldb_table('block_quickmail_config');
 
-        // Adding fields to table block_quickmail_config
+        // Adding fields to table block_quickmail_config.
         $table->add_field('id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
         $table->add_field('coursesid', XMLDB_TYPE_INTEGER, '11', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null);
         $table->add_field('name', XMLDB_TYPE_CHAR, '25', null, XMLDB_NOTNULL, null, null);
         $table->add_field('value', XMLDB_TYPE_CHAR, '125', null, null, null, null);
 
-        // Adding keys to table block_quickmail_config
+        // Adding keys to table block_quickmail_config.
         $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
 
-        // Conditionally launch create table for block_quickmail_config
+        // Conditionally launch create table for block_quickmail_config.
         if (!$dbman->table_exists($table)) {
             $dbman->create_table($table);
         }
 
-        // quickmail savepoint reached
+        // Quickmail savepoint reached.
         upgrade_block_savepoint($result, 2011021812, 'quickmail');
     }
 
@@ -151,7 +187,7 @@ function xmldb_block_quickmail_upgrade($oldversion) {
         }
 
         foreach (['log', 'drafts'] as $table) {
-            // Define field alternateid to be added to block_quickmail_log
+            // Define field alternateid to be added to block_quickmail_log.
             $table = new xmldb_table('block_quickmail_' . $table);
             $field = new xmldb_field(
                 'alternateid',
@@ -164,18 +200,18 @@ function xmldb_block_quickmail_upgrade($oldversion) {
                 'userid'
             );
 
-            // Conditionally launch add field alternateid
+            // Conditionally launch add field alternateid.
             if (!$dbman->field_exists($table, $field)) {
                 $dbman->add_field($table, $field);
             }
         }
 
-        // quickmail savepoint reached
+        // Quickmail savepoint reached.
         upgrade_block_savepoint($result, 2012021014, 'quickmail');
     }
 
     if ($oldversion < 2012061112) {
-        // Restructure database references to the new filearea locations
+        // Restructure database references to the new filearea locations.
         foreach (['log', 'drafts'] as $type) {
             $params = [
                 'component' => 'block_quickmail_' . $type,
--- db/upgradelib.php
+++ db/upgradelib.php
@@ -1,25 +1,39 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * Folder plugin version information
+ * Quickmail upgrade functions.
  *
- * @package
- * @subpackage
- * @copyright  block_quickmail
- * @author Celine Perves <cperves@unistra.fr>
+ * @package block_quickmail
+ * @copyright  2012 unistra  {@link http://unistra.fr}
+ * @author     Celine Perves <cperves@unistra.fr>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @license    http://www.cecill.info/licences/Licence_CeCILL_V2-en.html
  */
+
 /**
- * function to migrate quickmail history files attachment to the new file version from 1.9 to 2.x
+ * Function to migrate quickmail history files attachment to the new file version from 1.9 to 2.x.
  */
-function migrate_quickmail_20() {
+function migrate_quickmail_20(): void {
     global $DB;
-    // migration of attachments
+    // Migration of attachments.
     $fs = get_file_storage();
     $quickmaillogrecords = $DB->get_records_select('block_quickmail_log', 'attachment<>\'\'');
     foreach ($quickmaillogrecords as $quickmaillogrecord) {
-        // searching file into mdl_files
-        // analysing attachment content
+        // Searching file into mdl_files, analysing attachment content.
         $filename = $quickmaillogrecord->attachment;
         $filepath = '';
         $notrootfile = strstr($quickmaillogrecord->attachment, '/');
@@ -36,14 +50,22 @@ function migrate_quickmail_20() {
         $coursefile = $fs->get_file($coursecontext->id, 'course', 'legacy', 0, $filepath, $filename);
         if ($coursefile) {
             if ($notrootfile) {
-                // rename
+                // Rename file.
                 $filename = str_replace('/', '_', $quickmaillogrecord->attachment);
                 $filepath = '/';
                 $quickmaillogrecord->attachment = $filename;
                 $DB->update_record('block_quickmail_log', $quickmaillogrecord);
             }
-            $filerecord = ['contextid' => $coursecontext->id, 'component' => 'block_quickmail', 'filearea' => 'attachment_log', 'itemid' => $quickmaillogrecord->id, 'filepath' => $filepath, 'filename' => $filename,
-                    'timecreated' => $coursefile->get_timecreated(), 'timemodified' => $coursefile->get_timemodified()];
+            $filerecord = [
+                'contextid' => $coursecontext->id,
+                'component' => 'block_quickmail',
+                'filearea' => 'attachment_log',
+                'itemid' => $quickmaillogrecord->id,
+                'filepath' => $filepath,
+                'filename' => $filename,
+                'timecreated' => $coursefile->get_timecreated(),
+                'timemodified' => $coursefile->get_timemodified(),
+            ];
             if (!$fs->file_exists($coursecontext->id, 'block_quickmail', 'attachment_log', 0, $filepath, $filename)) {
                 $fs->create_file_from_storedfile($filerecord, $coursefile->get_id());
             }
