303a04adc2b76aeedd27a43b7e346867a78c994c
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2025-05-06 16:02:51 -0700

    make the signature column in the block_quickmail_signatures table non nullable.

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025022101;
+$plugin->version = 2025022102;
 $plugin->requires = 2024100100;
 $plugin->release = "v4.5";
 $plugin->supported = [405, 405];
